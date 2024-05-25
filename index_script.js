510444929e218948ab26c073d6506b70d70c63ff
Chanmonyreach <131956359+Chanmonyreach@users.noreply.github.com>
2024-05-26 01:56:04 +0700

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -1,83 +1,84 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sidenavElement = document.getElementById('sidenav');
     const accountBtnTopbar = document.getElementById('menuBtn');
-    const accountBtnSidebar = document.getElementById('accountBtnSidebar');
     const topnav = document.getElementById('topnav');
-    const menuBtn = document.getElementById("menuBtn");
     const loginForm = document.getElementById('loginForm');
     const registerForm = document.getElementById('registerForm');
 
     // Set initial styles for elements
-    sidenavElement.style.right = "-250px";
+    sidenavElement.style.right = "-40%";
 
     // Event listener for sidebar toggle button
-    menuBtn.onclick = function() {
-        if (sidenavElement.style.right == "-250px") {
-            sidenavElement.style.right = "0px";
-        } else {
-            sidenavElement.style.right = "-250px";
-        }
-    };
-
-    // Event listener for topbar account button
-    accountBtnTopbar.onclick = function() {
-        if (sidenavElement.style.right === "-250px") {
-            sidenavElement.style.right = "0px";
-            topnav.style.right = "280px"; // Adjust the value based on the width of the sidebar
-            accountBtnTopbar.style.display = "none";
-            accountBtnSidebar.style.display = "block";
-        } else {
-            sidenavElement.style.right = "-250px";
-            topnav.style.right = "30px"; // Reset the position of the topnav when the sidebar is closed
-            accountBtnTopbar.style.display = "block";
-            accountBtnSidebar.style.display = "none";
-        }
-    };
+    if (accountBtnTopbar) {
+        accountBtnTopbar.onclick = function() {
+            if (sidenavElement.style.right === "-40%") {
+                sidenavElement.style.right = "0";
+                topnav.style.right = "44%";
+                accountBtnTopbar.style.display = "none";
+            } else {
+                sidenavElement.style.right = "-40%";
+                topnav.style.right = "4%";
+                accountBtnTopbar.style.display = "block";
+            }
+        };
+    }
 
     // Close the sidebar when clicking outside of it
     document.addEventListener('click', function(e) {
         if (!sidenavElement.contains(e.target) && !accountBtnTopbar.contains(e.target)) {
-            sidenavElement.style.right = "-250px";
-            topnav.style.right = "30px"; // Reset the position of the topnav when the sidebar is closed
+            sidenavElement.style.right = "-40%";
+            topnav.style.right = "30px";
             accountBtnTopbar.style.display = "block";
-            accountBtnSidebar.style.display = "none";
         }
     });
 
     // Function to toggle the display of the login form
-    document.getElementById('loginBtn').onclick = function() {
-        if (loginForm.style.display !== 'block') {
-            loginForm.style.display = 'block';
-            registerForm.style.display = 'none';
-        } else if (registerForm.style.display === 'block') {
-            registerForm.style.display = 'none';
+    if (document.getElementById('loginBtn')) {
+        document.getElementById('loginBtn').onclick = function() {
+            if (loginForm.style.display !== 'block') {
+                loginForm.style.display = 'block';
+                registerForm.style.display = 'none';
+                accountBtnTopbar.style.display = "block"; // Ensure the menu icon is visible
+            } else if (registerForm.style.display === 'block') {
+                registerForm.style.display = 'none';
+            }
         }
     }
 
     // Function to toggle the display of the register form
-    document.getElementById('sidenavRegisterBtn').onclick = function() {
-        if (sidenavElement.style.right === "0px" && registerForm.style.display !== 'block') {
-            registerForm.style.display = 'block';
-            loginForm.style.display = 'none';
-            sidenavElement.style.right = "-250px"; // Close the sidebar
-        } else if (loginForm.style.display === 'block') {
-            loginForm.style.display = 'none';
+    if (document.getElementById('sidenavRegisterBtn')) {
+        document.getElementById('sidenavRegisterBtn').onclick = function() {
+            if (sidenavElement.style.right === "0px" && registerForm.style.display !== 'block') {
+                registerForm.style.display = 'block';
+                loginForm.style.display = 'none';
+                sidenavElement.style.right = "-250px";
+                accountBtnTopbar.style.display = "block"; // Ensure the menu icon is visible
+            } else if (loginForm.style.display === 'block') {
+                loginForm.style.display = 'none';
+            }
         }
     }
 
     // Close the forms when clicking outside of them
     document.addEventListener('click', function(event) {
-        var forms = document.getElementsByClassName('form-container');
+        var Login_forms = document.getElementsByClassName('form-containerLog');
+        var Register_forms = document.getElementsByClassName('form-containerReg');
         var loginBtn = document.getElementById('loginBtn');
         var registerBtn = document.getElementById('sidenavRegisterBtn');
         var clickedElement = event.target;
         var isLoginForm = (clickedElement.id === 'loginBtn' || clickedElement === loginBtn);
         var isRegisterForm = (clickedElement.id === 'sidenavRegisterBtn' || clickedElement === registerBtn);
 
-        for (var i = 0; i < forms.length; i++) {
-            var form = forms[i];
-            if (event.target != form && !form.contains(event.target) && !isLoginForm && !isRegisterForm) {
-                form.style.display = 'none';
+        for (var i = 0; i < Login_forms.length; i++) {
+            var Login_form = Login_forms[i];
+            if (event.target != Login_form && !Login_form.contains(event.target) && !isLoginForm && !isRegisterForm) {
+                Login_form.style.display = 'none';
+            }
+        }
+        for (var i = 0; i < Register_forms.length; i++) {
+            var Register_form = Register_forms[i];
+            if (event.target != Register_form && !Register_form.contains(event.target) && !isLoginForm && !isRegisterForm) {
+                Register_form.style.display = 'none';
             }
         }
     });
